20ab3447ac896f9b148c6f2e2f31d9859e1377c5
kflmiami420 <42599019+kflmiami420@users.noreply.github.com>
2018-11-24 18:38:13 -0500

    Create dht22pin22log2.py

---
+++ dht22pin22log2.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/python
+from __future__ import division
+import Adafruit_DHT
+import time
+
+def avg_min_max(data):
+    """Given a list, return a tuple of the mean, min, and max."""
+    return sum(data) / len(data), min(data), max(data)
+
+# Configuration
+sensor = Adafruit_DHT.DHT22
+gpio = 22
+count = 5
+
+date = time.gmtime()
+humid_temp_data = [Adafruit_DHT.read_retry(sensor, gpio) for _ in range(count)]
+humid_stats = avg_min_max([humid for humid, _ in humid_temp_data])
+temp_stats = avg_min_max([temp for _, temp in humid_temp_data])
+
+with open("MainLog.txt", "a") as file:
+    file.write(time.strftime("%Y-%m-%d %H:%M:%S: ", date))
+    file.write("{0},{1},{2} | {3},{4},{5}\n".format(*(humid_stats + temp_stats)))
+
