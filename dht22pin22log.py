5fe2b5297824b1f83b31b12c86189782e073aa9f
kflmiami420 <42599019+kflmiami420@users.noreply.github.com>
2018-11-24 18:36:45 -0500

    Create dht22pin22log.py

---
+++ dht22pin22log.py
@@ -0,0 +1,21 @@
+
+#pin 22
+#!/usr/bin/python
+import time
+import sys
+import Adafruit_DHT
+
+while True:
+
+    data =  humidity, temperature = Adafruit_DHT.read_retry(22,22)
+    temperature = temperature * 9/5.0 + 32
+    layout = '{0:5d}:  {1},  {0:0.1f} deg F,  {1:0.1f} humidity %'
+    counter = 1
+
+    with open("sample.log","a+") as f:
+        f.write(layout.format(counter, data.timestamp, data.temperature,  data.humidity) + "\n")
+    f.close()
+    counter += 1
+    time.sleep(5)
+#    print 'Temp: {0:0.1f} F  Humidity: {1:0.1f} %'.format(temperature, humidity)
+
