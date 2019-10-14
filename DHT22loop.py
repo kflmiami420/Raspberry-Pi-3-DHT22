d1c45a6e6bff99ca635ce1dca32371d2f06508b0
KFLMIAMI420 <42599019+kflmiami420@users.noreply.github.com>
2019-10-14 15:58:24 -0400

    Update DHT22loop.py

--- DHT22loop.py
+++ DHT22loop.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-#dht22 on pin 4 
+#dht22 on pin 10
 
 import sys
 import Adafruit_DHT
 
 while True:
 
-    humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(22, 10)
     temperature = temperature * 9/5.0 + 32
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
 
