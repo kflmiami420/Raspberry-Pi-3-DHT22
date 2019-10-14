630523550a8d06d425490fe15483f7dbc9376222
KFLMIAMI420 <42599019+kflmiami420@users.noreply.github.com>
2019-10-14 15:53:45 -0400

    Update dht22pin22log.py

--- dht22pin22log.py
+++ dht22pin22log.py
@@ -1,5 +1,5 @@
 
-#pin 22
+#pin 10
 #!/usr/bin/python
 import time
 import sys
@@ -7,12 +7,12 @@ import Adafruit_DHT
 
 while True:
 
-    data =  humidity, temperature = Adafruit_DHT.read_retry(22,22)
+    data =  humidity, temperature = Adafruit_DHT.read_retry(22,10)
     temperature = temperature * 9/5.0 + 32
     layout = '{0:5d}:  {1},  {0:0.1f} deg F,  {1:0.1f} humidity %'
     counter = 1
 
-    with open("sample.log","a+") as f:
+    with open("dht22.log","a+") as f:
         f.write(layout.format(counter, data.timestamp, data.temperature,  data.humidity) + "\n")
     f.close()
     counter += 1
